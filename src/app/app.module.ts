e1e52f34ca5089093e178ce155c058ad95d6ce98
AldinBe <aldinbecic1996@gmail.com>
2018-10-31 18:08:16 +0100

    posts fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -10,6 +11,12 @@ import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { PostsModule } from './posts/posts.module';
+
+const routes: Routes =[
+  {path : '', redirectTo:'/blog', pathMatch:'full'},
+  {path : '', loadChildren:'./posts/posts.module#PostsModule'}
+]
 
 @NgModule({
   declarations: [
@@ -18,12 +25,14 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    PostsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
