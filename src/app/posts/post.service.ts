8b747199f3fe76e47cae76a4b5ec41d88998f468
AldinBe <aldinbecic1996@gmail.com>
2018-11-01 21:46:39 +0100

    Post list

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Post } from './post'
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
+  postsCollection: AngularFirestoreCollection<Post>
+  postDoc: AngularFirestoreDocument<Post>
 
-  constructor() { }
+
+  constructor(private afs: AngularFirestore) {
+    this.postsCollection = this.afs.collection('posts', ref =>
+      ref.orderBy('published', 'desc')
+    )
+  }
+  getPosts() {
+    return this.postsCollection.snapshotChanges().map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data() as Post
+        const id = a.payload.doc.id
+        return { id, ...data }
+      })
+    })
+  }
+
 }
