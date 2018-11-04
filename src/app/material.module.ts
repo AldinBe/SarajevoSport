b368bf9edb7d66c38cb20b416d77c1fa800fe641
AldinBe <aldinbecic1996@gmail.com>
2018-11-04 17:48:35 +0100

    input form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
   imports: [
     MatButtonModule,
-    MatToolbarModule
+    MatInputModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   exports: [
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatInputModule,
+    MatExpansionModule
   ]
 })
 export class MaterialModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import { from } from 'rxjs';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     MaterialModule,
     RouterModule
   ],
   exports: [
     CommonModule,
+    FormsModule,
     MaterialModule,
     NavbarComponent,
     RouterModule],
